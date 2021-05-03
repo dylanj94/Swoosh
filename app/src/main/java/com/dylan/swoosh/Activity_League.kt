d7c8b11a022f750ef68d4692bb463fcd19c0bd93
Dylan Jones <dylanjones500@gmail.com>
2021-05-03 13:25:18 -0600

    Lesson 2

---
+++ app/src/main/java/com/dylan/swoosh/Activity_League.kt
@@ -0,0 +1,11 @@
+package com.dylan.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_League : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity__league)
+    }
+}
