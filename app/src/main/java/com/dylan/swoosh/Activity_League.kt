74ac914966a988b4d0a866a7fc4fa36ed41c5e15
Dylan Jones <dylanjones500@gmail.com>
2021-05-03 15:45:39 -0600

    Lesson 6

--- app/src/main/java/com/dylan/swoosh/Activity_League.kt
+++ app/src/main/java/com/dylan/swoosh/Activity_League.kt
@@ -1,13 +1,21 @@
 package com.dylan.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Activity_League : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity__league)
 
+
+    }
+
+    fun leagueNextClicked(view: View) {
+        val skillActivity = Intent(this, SkillActivity::class.java)
+        startActivity(skillActivity)
 
     }
 }
