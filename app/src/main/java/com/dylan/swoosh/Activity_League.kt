8f1e5fc4e44188449fc6bcfd1ad38459a45ee2ca
Dylan Jones <dylanjones500@gmail.com>
2021-05-03 14:40:03 -0600

    Lesson 3

--- app/src/main/java/com/dylan/swoosh/Activity_League.kt
+++ app/src/main/java/com/dylan/swoosh/Activity_League.kt
@@ -7,5 +7,7 @@ class Activity_League : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity__league)
+
+
     }
 }
--- app/src/main/java/com/dylan/swoosh/MainActivity.kt
+++ app/src/main/java/com/dylan/swoosh/MainActivity.kt
@@ -2,11 +2,22 @@ package com.dylan.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.content.Intent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Added this comment
+        val button = findViewById<Button>(R.id.getStarted)
+        button.setOnClickListener {
+            val leagueIntent = Intent(this, Activity_League::class.java)
+            startActivity(leagueIntent)
+        }
+
     }
+
+
+
+
 }
